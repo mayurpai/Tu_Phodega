8bf6731869af92bb95c4d47fdfb508b06806a1cf
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-12-01 11:35:59 +0530

    Link Fail

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,16 @@
 import "../styles/Card.css";
 
 import React from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import { Switch } from "react-dom";
 
 const Card = (props) => {
   return (
     <div className="Card">
       <div className="CompDiv">
         <div className={props.class}>
-          <Router>
-            <Switch>
-              <Route path="/Error" component={Error} exact />
-              <Link to="/Error" Title={props.name}>
-                <h1>{props.name}</h1>
-                <img src={props.image} alt={props.name} />
-              </Link>
-            </Switch>
-          </Router>
+          <a href="/Error" Title={props.name}>
+            <h1>{props.name}</h1>
+            <img src={props.image} alt={props.name} />
+          </a>
         </div>
       </div>
     </div>
